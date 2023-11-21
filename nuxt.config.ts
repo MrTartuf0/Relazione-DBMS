1d4c98db5bd485ca4f502f27dc6419d662d76432
Vitaliy Didyk <79630556+MrTartuf0@users.noreply.github.com>
2023-11-21 10:33:38 -0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,9 @@ export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: '@nuxt-themes/docus',
   devtools: { enabled: true },
-
+  css: [
+    '@/assets/css/global.css', // Path to your global CSS file
+  ],
   modules: [
     // Remove it if you don't use Plausible analytics
     // https://github.com/nuxt-modules/plausible
